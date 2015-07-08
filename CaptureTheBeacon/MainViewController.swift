793e538184eea9c144602dab45faeb6c935ac1f5
dvermeir <david.vermeir@iminds.be>
2015-07-08 11:31:02 +0200

    Showing player name and team

--- CaptureTheBeacon/MainViewController.swift
+++ CaptureTheBeacon/MainViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 import Parse
 
 class MainViewController: UIViewController {
+    
+    var currentUser:PFUser!
 
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var playerLabel: UILabel!
+    @IBOutlet weak var playerTeam: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.currentUser = PFUser.currentUser()!
+        //self.playerLabel.text = self.currentUser["nick"]
+        //let teamName = "team " + currentUser["team"] as String
+        //self.playerTeam.text = teamName
+        
+        switch currentUser["team"] as! String {
+            case "red":
+                self.playerTeam.textColor = UIColor.redColor()
+            case "blue":
+                self.playerTeam.textColor = UIColor.blueColor()
+            default:
+                self.playerTeam.textColor = UIColor.blackColor()
+        }
+        
+        let leftConstraint = NSLayoutConstraint(
+            item:self.contentView,
+            attribute:NSLayoutAttribute.Leading,
+            relatedBy:NSLayoutRelation.Equal,
+            toItem:self.view,
+            attribute:NSLayoutAttribute.Left,
+            multiplier:1.0,
+            constant:0
+        )
+        self.view.addConstraint(leftConstraint);
+        
+        let rightConstraint = NSLayoutConstraint(
+            item:self.contentView,
+            attribute:NSLayoutAttribute.Trailing,
+            relatedBy:NSLayoutRelation.Equal,
+            toItem:self.view,
+            attribute:NSLayoutAttribute.Right,
+            multiplier:1.0,
+            constant:0
+        )
+        self.view.addConstraint(rightConstraint)
+        
     }
 
     override func didReceiveMemoryWarning() {
